70527f0697fb7a4c81143a9b1a6387df35e2b18d
KrolPolski <ryanboudwin@outlook.com>
2023-04-11 11:13:34 +0300

    added resize functionality project complete

--- etch-and-sketch.js
+++ etch-and-sketch.js
@@ -1,13 +1,17 @@
 const container = document.querySelector(".container");
 // generate 16 x 16 grid
-for (i = 16; i > 0; i--)
-{
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add("rowDiv");
-    //rowDiv.textContent = "hi";
-    container.appendChild(rowDiv);
+let gridSize = 16;
+
+function drawGrid()
+{ 
+    for (i = gridSize; i > 0; i--)
+    {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add("rowDiv");
+        //rowDiv.textContent = "hi";
+        container.appendChild(rowDiv);
     
-        for (j = 16; j > 0; j--)
+        for (j = gridSize; j > 0; j--)
         {
             const sqDiv = document.createElement('div');
             sqDiv.classList.add("sqDiv");
@@ -16,6 +20,23 @@ for (i = 16; i > 0; i--)
                 sqDiv.classList.add("hovered");});
         }
 }
+}
 
+drawGrid();
+// New Grid prompt button
+const btnNewGrid = document.querySelector("#NewGrid");
+btnNewGrid.addEventListener('click', function()
+{
+    let newGridSize = parseInt(prompt("How wide a grid would you like, in squares?"));
+    console.log(newGridSize);
+    // input validation
+    if (isNaN(newGridSize) == false && 1 <= newGridSize && newGridSize <= 100)
+    {
+        gridSize = newGridSize;
+        container.innerHTML = "";
+        drawGrid();
+    }
+    else alert("that's not a number between 1 and 100");
+});
 
 
