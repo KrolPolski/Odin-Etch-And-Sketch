6e38cc1698afb887a803c6762da96d53eb2589bc
KrolPolski <ryanboudwin@outlook.com>
2023-04-11 10:24:51 +0300

    grid now proportionally square

--- etch-and-sketch.js
+++ etch-and-sketch.js
@@ -4,14 +4,14 @@ for (i = 16; i > 0; i--)
 {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add("rowDiv");
-    rowDiv.textContent = "hi";
+    //rowDiv.textContent = "hi";
     container.appendChild(rowDiv);
     
         for (j = 16; j > 0; j--)
         {
             const sqDiv = document.createElement('div');
             sqDiv.classList.add("sqDiv");
-            sqDiv.textContent = "ho";
+            //sqDiv.textContent = "ho";
             rowDiv.appendChild(sqDiv);
         }
 }
